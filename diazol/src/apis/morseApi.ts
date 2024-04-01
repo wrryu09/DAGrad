02786a8add4a11c5e264b42c98725d096324a623
wrryu09 <rryu09@ewhain.net>
2024-04-01 22:51:01 +0900

    ⚡️ getMorseCode예외처리

--- diazol/src/apis/morseApi.ts
+++ diazol/src/apis/morseApi.ts
@@ -2,8 +2,12 @@ import axios from "axios";
 
 /** get morse code from english text */
 export const getMorseCode = async (text: string) => {
-  const morseCode = await axios.get(
-    `https://api.funtranslations.com/translate/morse.json?text=${text}`
-  );
-  return morseCode.data.contents.translated;
+  try {
+    const morseCode = await axios.get(
+      `https://api.funtranslations.com/translate/morse.json?text=${text}`
+    );
+    return morseCode.data.contents.translated;
+  } catch (e) {
+    console.log(e);
+  }
 };
