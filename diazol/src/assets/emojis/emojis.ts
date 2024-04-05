083b467ce87c84d03e955a7110b917ccda2f5574
wrryu09 <rryu09@ewhain.net>
2024-04-05 14:34:56 +0900

    ✨ add emoji sample data

---
+++ diazol/src/assets/emojis/emojis.ts
@@ -0,0 +1,15 @@
+import sampleImg from "./sample.webp";
+export const emojis = {
+  sampleImg: sampleImg,
+  sampleImg1: sampleImg,
+  sampleImg2: sampleImg,
+  sampleImg3: sampleImg,
+  sampleImg4: sampleImg,
+  sampleImg5: sampleImg,
+  sampleImg6: sampleImg,
+  sampleImg7: sampleImg,
+  sampleImg8: sampleImg,
+  sampleImg9: sampleImg,
+  sampleImg10: sampleImg,
+  sampleImg11: sampleImg,
+};
