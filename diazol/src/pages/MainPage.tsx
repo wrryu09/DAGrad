4fa5f893824580e2fc1b223252de6c0dfc1588a2
wrryu09 <rryu09@ewhain.net>
2024-04-12 20:26:46 +0900

    ✨ add logo image

--- diazol/src/pages/MainPage.tsx
+++ diazol/src/pages/MainPage.tsx
@@ -1,15 +1,22 @@
 "use client";
 import MainBtn from "@/components/common/MainBtn";
+import Image from "next/image";
+import bpm_logo from "../assets/images/bpm_logo.png";
 import React from "react";
 
 type Props = {};
 
 const MainPage = (props: Props) => {
   return (
-    <div className="flex flex-col">
-      <p>LogoImg</p>
+    <div className="flex flex-col items-center">
+      <Image src={bpm_logo} alt="logo" priority className="mt-32 w-40 h-auto" />
+      <h1 className="text-white font-bold text-2xl">BPM</h1>
       <div className="flex justify-center">
-      <MainBtn text="나의 BPM 체크하기" available={true} route="survey/type" />
+        <MainBtn
+          text="나의 BPM 체크하기"
+          available={true}
+          route="survey/type"
+        />
       </div>
     </div>
   );
