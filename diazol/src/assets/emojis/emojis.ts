ce91a37744e66c425e37eac0a5d072bb06df6241
wrryu09 <rryu09@ewhain.net>
2024-04-12 20:26:20 +0900

    ✨ add emoji images

--- diazol/src/assets/emojis/emojis.ts
+++ diazol/src/assets/emojis/emojis.ts
@@ -1,15 +1,27 @@
-import sampleImg from "./sample.webp";
+import emo_1 from "./emo_1.png";
+import emo_2 from "./emo_2.png";
+import emo_3 from "./emo_3.png";
+import emo_4 from "./emo_4.png";
+import emo_5 from "./emo_5.png";
+import emo_6 from "./emo_6.png";
+import emo_7 from "./emo_7.png";
+import emo_8 from "./emo_8.png";
+import emo_9 from "./emo_9.png";
+import emo_10 from "./emo_10.png";
+import emo_11 from "./emo_11.png";
+import emo_12 from "./emo_12.png";
+
 export const emojis = {
-  sampleImg: sampleImg,
-  sampleImg1: sampleImg,
-  sampleImg2: sampleImg,
-  sampleImg3: sampleImg,
-  sampleImg4: sampleImg,
-  sampleImg5: sampleImg,
-  sampleImg6: sampleImg,
-  sampleImg7: sampleImg,
-  sampleImg8: sampleImg,
-  sampleImg9: sampleImg,
-  sampleImg10: sampleImg,
-  sampleImg11: sampleImg,
+  emoIcon1: emo_1,
+  emoIcon2: emo_2,
+  emoIcon3: emo_3,
+  emoIcon4: emo_4,
+  emoIcon5: emo_5,
+  emoIcon6: emo_6,
+  emoIcon7: emo_7,
+  emoIcon8: emo_8,
+  emoIcon9: emo_9,
+  emoIcon10: emo_10,
+  emoIcon11: emo_11,
+  emoIcon12: emo_12,
 };
