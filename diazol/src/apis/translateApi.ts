c65df1467abf248a9df07fe07ee0de4160d7f906
wrryu09 <rryu09@ewhain.net>
2024-04-12 11:28:19 +0900

    ⚡️ get romanized text from my server

--- diazol/src/apis/translateApi.ts
+++ diazol/src/apis/translateApi.ts
@@ -1,24 +1,15 @@
 import axios from "axios";
-
 /** get romanized text */
 export const getRomanWord = async (text: string) => {
   try {
     const translated = await axios.get(
-      `${process.env.NEXT_PUBLIC_HEROKU}https://naveropenapi.apigw.ntruss.com/krdict/v1/romanization?query=${text}`,
-      {
-        headers: {
-          "X-NCP-APIGW-API-KEY-ID":
-            process.env.NEXT_PUBLIC_X_NCP_APIGW_API_KEY_ID,
-          "X-NCP-APIGW-API-KEY": process.env.NEXT_PUBLIC_X_NCP_APIGW_API_KEY,
-        },
-      }
+      `${process.env.NEXT_PUBLIC_MY_SERVER}/romanize?text=${text}`
     );
-    console.log(translated.data.aResult[0].aItems[0].name);
+    console.log(translated.data.translated);
 
-    return translated.data.aResult[0].aItems[0].name;
+    return translated.data.translated;
   } catch (e) {
     console.log(e);
-
     // romanization 실패시 exception 스트링 반환
     return "exception";
   }
