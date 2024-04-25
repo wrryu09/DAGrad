0f9780d6726cde9c6b59daf56d5ee4d9bec2254a
wrryu09 <rryu09@ewhain.net>
2024-04-26 01:10:06 +0900

    ✨ add sample qrimage

--- diazol/src/assets/images/smage/smage.ts
+++ diazol/src/assets/images/smage/smage.ts
@@ -8,6 +8,16 @@ import high1 from "./high1.webp";
 import high2 from "./high2.webp";
 import high3 from "./high3.webp";
 
+import low1qr from "./low1qr.png";
+import low2qr from "./low2qr.png";
+import low3qr from "./low3qr.png";
+import mid1qr from "./mid1qr.png";
+import mid2qr from "./mid2qr.png";
+import mid3qr from "./mid3qr.png";
+import high1qr from "./high1qr.png";
+import high2qr from "./high2qr.png";
+import high3qr from "./high3qr.png";
+
 export const smage = {
   low1: low1,
   low2: low2,
@@ -18,4 +28,16 @@ export const smage = {
   high1: high1,
   high2: high2,
   high3: high3,
+};
+
+export const smageQr = {
+  low1: low1qr,
+  low2: low2qr,
+  low3: low3qr,
+  mid1: mid1qr,
+  mid2: mid2qr,
+  mid3: mid3qr,
+  high1: high1qr,
+  high2: high2qr,
+  high3: high3qr,
 };
