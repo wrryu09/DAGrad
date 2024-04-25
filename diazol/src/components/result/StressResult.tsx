e0d1ca99bee407d03d1a550343c3f052809e8013
wrryu09 <rryu09@ewhain.net>
2024-04-26 00:49:41 +0900

    ✨ 결과페이지 샘플 스미지 추가

--- diazol/src/components/result/StressResult.tsx
+++ diazol/src/components/result/StressResult.tsx
@@ -1,19 +1,32 @@
 import { stressInfo } from "@/data";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InfoBox from "./InfoBox";
 import { useData } from "@/utils/DataContext";
+import Image from "next/image";
+import { getStressNickName } from "@/utils/getStressNickname";
+import { NickNameType } from "@/types";
+import { smage } from "@/assets/images/smage/smage";
 
 type Props = {};
 
 const StressResult = (props: Props) => {
-  const { data, setData } = useData();
+  const { data } = useData();
+  const [nickname, setNickname] = useState<NickNameType>({
+    name: "Prestissimo",
+    content: "",
+    image: smage.high1,
+  });
+
+  useEffect(() => {
+    getStressNickName(setNickname, data.choiceScore, data.stressType);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center text-center">
+      <Image alt="smage" src={nickname.image} />
       <h1 className="text-3xl font-semibold mt-20 text-white leading-10">
-        {/* <p>stressType : {data.stressType}</p>
-        <p>emojiNum : {data.emojiNum}</p>
-        <p>stressWord : {data.stressWord}</p> */}
-
+        <h2>{nickname.name}</h2>
+        <p>{nickname.content}</p>
         <p>당신의 스트레스 점수는</p>
         <p>{data.choiceScore}점</p>
       </h1>
--- diazol/src/types.ts
+++ diazol/src/types.ts
@@ -1,10 +1,29 @@
-type QuestionDataType = {
+import { StaticImageData } from "next/image";
+
+export type QuestionDataType = {
   title: string;
   content: string;
   choice: number;
 };
 
-interface UserDataType {
+export type NickNameType = {
+  name: string;
+  content: string;
+  image: StaticImageData;
+};
+
+export type NickNameIdxType =
+  | "low1"
+  | "low2"
+  | "low3"
+  | "mid1"
+  | "mid2"
+  | "mid3"
+  | "high1"
+  | "high2"
+  | "high3";
+
+export interface UserDataType {
   stressType: number;
   choiceScore: number;
   emojiNum: number;
