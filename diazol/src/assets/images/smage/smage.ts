f5d3a9f4530c710d483ba7c7975bab94db05cbd1
wrryu09 <rryu09@ewhain.net>
2024-04-26 00:42:33 +0900

    ✨ add sample smage data

---
+++ diazol/src/assets/images/smage/smage.ts
@@ -0,0 +1,21 @@
+import low1 from "./low1.webp";
+import low2 from "./low2.webp";
+import low3 from "./low3.webp";
+import mid1 from "./mid1.webp";
+import mid2 from "./mid2.webp";
+import mid3 from "./mid3.webp";
+import high1 from "./high1.webp";
+import high2 from "./high2.webp";
+import high3 from "./high3.webp";
+
+export const smage = {
+  low1: low1,
+  low2: low2,
+  low3: low3,
+  mid1: mid1,
+  mid2: mid2,
+  mid3: mid3,
+  high1: high1,
+  high2: high2,
+  high3: high3,
+};
