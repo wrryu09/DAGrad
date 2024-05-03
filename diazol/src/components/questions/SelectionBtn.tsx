5ff151e6828df17acae5f97be22eb6b51ace5d4c
wrryu09 <rryu09@ewhain.net>
2024-05-03 15:42:49 +0900

    💄 fix input style

--- diazol/src/components/questions/SelectionBtn.tsx
+++ diazol/src/components/questions/SelectionBtn.tsx
@@ -9,25 +9,24 @@ type Props = {
 
 const SelectionBtn = (props: Props) => {
   const selectedStyle = "h-16 border border-diazolRed rounded-full text-white";
-  const defaultStyle =
-    "h-16 border rounded-full text-midGrey backdrop-blur-sm bg-[rgba(255, 255, 255, 0.10)]";
+  const defaultStyle = "h-16 border rounded-full text-midGrey";
 
   return (
     <div
-      className={`w-10/12 self-center flex items-center justify-center font-semibold`}
+      className={`w-11/12 self-center flex items-center justify-center font-semibold`}
       onClick={() => {
         props.setSelection(props.num);
       }}
     >
       <div
-        className={`w-full flex items-center justify-center ${
+        className={`w-full flex items-center justify-center bg-white/opacity-5 backdrop-blur-md ${
           props.selection == props.num ? selectedStyle : defaultStyle
         }`}
       >
         <p className="p-6">{props.text}</p>
       </div>
       <div
-        className={`w-20 ${
+        className={`w-20 bg-white/opacity-5 backdrop-blur-md ${
           props.selection == props.num ? selectedStyle : defaultStyle
         }`}
       ></div>
--- diazol/src/components/questions/TextInput.tsx
+++ diazol/src/components/questions/TextInput.tsx
@@ -5,8 +5,8 @@ type Props = { onChange: (e: React.ChangeEvent<HTMLInputElement>) => void };
 const TextInput = (props: Props) => {
   return (
     <input
-      className="h-[6.5rem] w-9/12 bg-midGrey rounded-md text-4xl p-4 placeholder:text-darkGrey text-center"
-      placeholder="ㅠㅠ"
+      className="h-[6.5rem] w-9/12 bg-[rgba(255,255,255,0.1)] backdrop-blur-lg border border-lightGrey rounded-full text-4xl p-4 placeholder:text-[rgb(135,135,135)] text-center text-white"
+      placeholder="입력!"
       maxLength={12}
       onChange={props.onChange}
     ></input>
