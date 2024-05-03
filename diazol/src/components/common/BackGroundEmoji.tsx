95a54cc186c519b698d6327d54040ff3fc15b74d
wrryu09 <rryu09@ewhain.net>
2024-05-03 15:30:49 +0900

    🐛 fix bgEmoji position

--- diazol/src/components/common/BackGroundEmoji.tsx
+++ diazol/src/components/common/BackGroundEmoji.tsx
@@ -5,24 +5,24 @@ type Props = {};
 
 const BackGroundEmoji = (props: Props) => {
   return (
-    <div className="w-full h-full max-w-[360px] -z-10">
+    <div className="absolute w-full h-full overflow-clip -z-10">
       <Image
         src={bgImg}
         alt="background image"
         width={400}
-        className="absolute -right-36 -top-12 rotate-[30deg] -z-10"
+        className="-right-36 -top-12 rotate-[30deg] translate-x-60 -translate-y-24 -z-10"
       />
       <Image
         src={bgImg}
         alt="background image"
         width={200}
-        className="absolute -left-10 top-[50%] -rotate-[24deg] -z-10"
+        className="-left-10 top-[50%] -rotate-[24deg] -translate-x-[60px] -z-10"
       />
       <Image
         src={bgImg}
         alt="background image"
         width={400}
-        className="absolute -right-12 -bottom-24 rotate-[10deg] -z-10"
+        className="-right-12 -bottom-24 rotate-[10deg] translate-x-60 -z-10"
       />
     </div>
   );
