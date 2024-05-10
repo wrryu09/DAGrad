a55b75247adc763b6df139f76023a0804aee99a9
wrryu09 <rryu09@ewhain.net>
2024-05-10 14:55:47 +0900

    🔧 local mp4 import setting

--- diazol/next.config.mjs
+++ diazol/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(mov|mp4)$/,
+      use: [
+        {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+            publicPath: "/_next/static/smageVid",
+            outputPath: "static/smageVid",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
