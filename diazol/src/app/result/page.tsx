c2bc8f29946078b3721ea1073f01b7a5be86e955
wrryu09 <rryu09@ewhain.net>
2024-05-16 11:42:43 +0900

    ♻️ : 모스부호 변환 함수 분리, changeToMorse 비활성화

--- diazol/src/app/result/page.tsx
+++ diazol/src/app/result/page.tsx
@@ -1,11 +1,11 @@
 "use client";
-import { getMorseCode } from "@/apis/morseApi";
-import { getRomanWord } from "@/apis/translateApi";
 import Spinner from "@/components/common/\bSpinner";
 import TopBar from "@/components/common/TopBar";
 import CheckCenter from "@/components/result/CheckCenter";
 import StressResult from "@/components/result/StressResult";
 import { useData } from "@/utils/DataContext";
+import changeToMorse from "@/utils/changeToMorse";
+import changeToStableMorse from "@/utils/changeToStableMorse";
 import React, { useEffect, useState } from "react";
 
 type Props = {};
@@ -15,25 +15,17 @@ const Page = (props: Props) => {
   const { data } = useData();
   const [morseCode, setMorseCode] = useState<string>("");
 
+  const handleMorseCode = (text: string) => setMorseCode(text);
+
   const showResult = () => {
     setShowRes(true);
   };
 
   useEffect(() => {
-    changeToMorse();
+    // changeToMorse(data.stressWord, handleMorseCode);
+    changeToStableMorse(data.stressWord, handleMorseCode);
   }, []);
 
-  const changeToMorse = async () => {
-    if (data.stressWord !== "-1") {
-      const romanStressWord = await getRomanWord(data.stressWord);
-      if (typeof romanStressWord === "string") {
-        const morseStressCode = await getMorseCode(romanStressWord);
-        setMorseCode(morseStressCode);
-      }
-    } else {
-      console.log("error: no stress word stored");
-    }
-  };
   return (
     <div>
       <TopBar />
