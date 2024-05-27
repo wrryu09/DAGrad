68e292694394099a81e8aea33b2761080c54de7a
wrryu09 <rryu09@ewhain.net>
2024-05-27 17:14:29 +0900

    🔥 remove trycatch

--- diazol/src/apis/morseApi.ts
+++ diazol/src/apis/morseApi.ts
@@ -1,16 +1,11 @@
 /** get morse code from english text */
 export const getMorseCode = (text: string) => {
   const { encode } = require("morsee");
-  try {
-    let morseCode = encode(text.trim());
-    morseCode = morseCode.trim();
-    if (morseCode.length < 1) {
-      console.log("dummy morse");
-      morseCode = ". .-. .-. --- .-. --";
-    }
-    return morseCode;
-  } catch (e) {
-    console.log(e, "error occured");
-    return ". .-. .-. --- .-. --- -.-. -.-. ..- .-. . -..";
+  let morseCode = encode(text.trim());
+  morseCode = morseCode.trim();
+  if (morseCode.length < 1) {
+    console.log("dummy morse");
+    morseCode = ". .-. .-. --- .-. --";
   }
+  return morseCode;
 };
