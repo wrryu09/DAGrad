cb484cb76589bed508165b0b50fd379862db3368
wrryu09 <rryu09@ewhain.net>
2024-05-27 16:26:44 +0900

    ✨ add link to TopBar

--- diazol/src/components/common/TopBar.tsx
+++ diazol/src/components/common/TopBar.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 type Props = {};
@@ -5,7 +6,9 @@ type Props = {};
 const TopBar = (props: Props) => {
   return (
     <div className="h-10 w-full bg-black flex items-center">
-      <div className="text-white px-4 font-bold text-lg">BPM</div>
+      <Link href={"/"} className="text-white px-4 font-bold text-lg">
+        BPM
+      </Link>
     </div>
   );
 };
