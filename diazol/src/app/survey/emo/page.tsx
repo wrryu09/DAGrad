8fae859bc21684cb4658088995ad086c6af4e4b9
wrryu09 <rryu09@ewhain.net>
2024-05-27 15:54:23 +0900

    💄 fix emo page UI

--- diazol/src/app/survey/emo/page.tsx
+++ diazol/src/app/survey/emo/page.tsx
@@ -18,14 +18,14 @@ const Page = (props: Props) => {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="w-9/12 flex flex-col items-center text-center">
-        <div className="text-white my-10">
+      <div className="w-10/12 flex flex-col items-center text-center">
+        <div className="text-white my-10 whitespace-pre-wrap">
           <h1>Part2</h1>
-          <p>짜증날 때 제일 거슬리는 소리의 형태가 있다면 무엇일까요?</p>
+          <p>짜증날 때 제일 거슬리는{"\n"}소리의 형태가 있다면 무엇일까요?</p>
           {/* {mySelection} */}
         </div>
 
-        <div className="w-fit flex flex-wrap gap-6 bg-[rgba(60,60,60,0.10)] backdrop-blur-lg p-4 py-8 rounded-lg justify-center">
+        <div className="w-full flex flex-wrap gap-6 bg-[rgba(60,60,60,0.10)] backdrop-blur-lg p-4 py-8 rounded-lg justify-center mb-80">
           {emojiArr.map((emo, idx) => (
             <Emoji
               idx={idx}
