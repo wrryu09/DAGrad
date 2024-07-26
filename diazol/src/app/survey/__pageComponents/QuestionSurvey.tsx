cac87119c673fa692e12884850c4b8d70e0c7afe
wrryu09 <rryu09@ewhain.net>
2024-07-27 02:44:00 +0900

    refactor: useEffect 사용 않도록 수정

--- diazol/src/app/survey/__pageComponents/QuestionSurvey.tsx
+++ diazol/src/app/survey/__pageComponents/QuestionSurvey.tsx
@@ -2,7 +2,7 @@
 import MainBtn from "@/components/common/MainBtn";
 import Question from "@/components/questions/Question";
 import { questions } from "@/data";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 type Props = {
   handleStressScore: (score: number) => void;
@@ -14,16 +14,15 @@ const QuestionSurvey = ({ handleStressScore, onNext }: Props) => {
   const [currentSelection, setCurrentSelection] = useState(-1);
   const [phase, setPhase] = useState(1);
   const order = [1, 2, 3, 6, 9, 10];
-  const [val, setVal] = useState(-1);
 
-  // set value via question order
-  useEffect(() => {
+  /** 문항별 점수 부여 */
+  const getValueOfSelection = (selection: number) => {
     if (order.includes(phase)) {
-      setVal(currentSelection);
+      return selection;
     } else {
-      setVal(4 - currentSelection);
+      return 4 - selection;
     }
-  }, [currentSelection, phase]);
+  };
 
   /** 총 점수 관리 */
   const handleScore = (sc: number) => {
@@ -33,9 +32,9 @@ const QuestionSurvey = ({ handleStressScore, onNext }: Props) => {
   /** 페이즈당 정보 관리 */
   const increasePhase = () => {
     if (phase + 1 <= 10) setPhase(phase + 1);
-    handleScore(val);
+    handleScore(getValueOfSelection(currentSelection));
     // state 변화 후 바로 사용
-    handleStressScore(val + score);
+    handleStressScore(getValueOfSelection(currentSelection) + score);
     setCurrentSelection(-1);
   };
 
@@ -46,7 +45,6 @@ const QuestionSurvey = ({ handleStressScore, onNext }: Props) => {
   return (
     <>
       <Question
-        Qnum={phase}
         title={questions["Q" + phase].title}
         content={questions["Q" + phase].content}
         currentSelection={currentSelection}
